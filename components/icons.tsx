92282a6f08364951fed63fbb38766094ecb799b7
eben96 <pappy.rhutmannjnrolege@gmail.com>
2024-06-29 22:48:42 +0000

    feat: Add SVG icons for collection, home, search, and sidebar

---
+++ components/icons.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import * as React from "react";
+import { SVGProps } from "react";
+
+export const Home = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={"24px"}
+    height={"24px"}
+    fill="none"
+    {...props}
+  >
+    <path
+      fill="#fff"
+      d="M25 27a1 1 0 0 1-1 1h-7.722a1 1 0 0 1-1-1v-7.947a1 1 0 0 0-1-1h-3.556a1 1 0 0 0-1 1V27a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1V7.898a1 1 0 0 1 .494-.863L11.996.295a1 1 0 0 1 1.009-.001l11.498 6.691A1 1 0 0 1 25 7.85V27Z"
+    />
+  </svg>
+);
