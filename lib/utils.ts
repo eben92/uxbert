434197363560bbe3a07c77e43547554d98b17941
eben96 <pappy.rhutmannjnrolege@gmail.com>
2024-06-30 18:32:35 +0000

    refactor: Remove unused code and update tracklist component

--- lib/utils.ts
+++ lib/utils.ts
@@ -11,3 +11,20 @@ export function secondsToMinutes(seconds: number) {
     .substr(14, 5)
     .replace(/^0+/, "");
 }
+
+export const formatTime = (seconds: number) => {
+  if (seconds === Infinity) {
+    return "--";
+  }
+  const floored = Math.floor(seconds);
+  let from = 14;
+  let length = 5;
+
+  // display hours only if necessary.
+  if (floored >= 3600) {
+    from = 11;
+    length = 8;
+  }
+
+  return new Date(floored * 1000).toISOString().substr(from, length);
+};
