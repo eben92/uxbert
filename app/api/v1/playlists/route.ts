f20ee19ed9bfe8cd3fbe577459a297a4b107f7be
eben96 <pappy.rhutmannjnrolege@gmail.com>
2024-06-30 09:55:08 +0000

    feat: Add API route for playlists

---
+++ app/api/v1/playlists/route.ts
@@ -0,0 +1,77 @@
+export async function GET() {
+  return Response.json({
+    data: {
+      data: [
+        {
+          id: "12860148981",
+          title: "22323",
+          duration: 0,
+          public: true,
+          is_loved_track: false,
+          collaborative: false,
+          nb_tracks: 0,
+          fans: 0,
+          link: "https://www.deezer.com/playlist/12860148981",
+          picture: "https://api.deezer.com/playlist/12860148981/image",
+          picture_small:
+            "https://e-cdns-images.dzcdn.net/images/cover//56x56-000000-80-0-0.jpg",
+          picture_medium:
+            "https://e-cdns-images.dzcdn.net/images/cover//250x250-000000-80-0-0.jpg",
+          picture_big:
+            "https://e-cdns-images.dzcdn.net/images/cover//500x500-000000-80-0-0.jpg",
+          picture_xl:
+            "https://e-cdns-images.dzcdn.net/images/cover//1000x1000-000000-80-0-0.jpg",
+          checksum: "4e281116259014241f9ec2d75c72774c",
+          tracklist: "https://api.deezer.com/playlist/12860148981/tracks",
+          creation_date: "2024-06-30 08:44:05",
+          md5_image: "",
+          picture_type: "cover",
+          time_add: 1719737045,
+          time_mod: 1719737045,
+          creator: {
+            id: "6134877461",
+            name: "eben",
+            tracklist: "https://api.deezer.com/user/6134877461/flow",
+            type: "user",
+          },
+          type: "playlist",
+        },
+        {
+          id: "12850928381",
+          title: "Loved Tracks",
+          duration: 0,
+          public: true,
+          is_loved_track: true,
+          collaborative: false,
+          nb_tracks: 0,
+          fans: 0,
+          link: "https://www.deezer.com/playlist/12850928381",
+          picture: "https://api.deezer.com/playlist/12850928381/image",
+          picture_small:
+            "https://e-cdns-images.dzcdn.net/images/cover/0bc34ce79169a66ace4b4fd553e13355/56x56-000000-80-0-0.jpg",
+          picture_medium:
+            "https://e-cdns-images.dzcdn.net/images/cover/0bc34ce79169a66ace4b4fd553e13355/250x250-000000-80-0-0.jpg",
+          picture_big:
+            "https://e-cdns-images.dzcdn.net/images/cover/0bc34ce79169a66ace4b4fd553e13355/500x500-000000-80-0-0.jpg",
+          picture_xl:
+            "https://e-cdns-images.dzcdn.net/images/cover/0bc34ce79169a66ace4b4fd553e13355/1000x1000-000000-80-0-0.jpg",
+          checksum: "80d1abd98b224226e5239365f4d24087",
+          tracklist: "https://api.deezer.com/playlist/12850928381/tracks",
+          creation_date: "2024-06-27 02:28:43",
+          md5_image: "0bc34ce79169a66ace4b4fd553e13355",
+          picture_type: "cover",
+          time_add: 1719455323,
+          time_mod: 1719481875,
+          creator: {
+            id: "6134877461",
+            name: "eben",
+            tracklist: "https://api.deezer.com/user/6134877461/flow",
+            type: "user",
+          },
+          type: "playlist",
+        },
+      ],
+      total: 2,
+    },
+  });
+}
