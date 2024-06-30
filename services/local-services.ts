04a9071ab48564dfb70bf95fdbc1e41817d6b093
eben96 <pappy.rhutmannjnrolege@gmail.com>
2024-06-30 20:22:24 +0000

    refactor: Update local-services.ts to improve session storage handling

--- services/local-services.ts
+++ services/local-services.ts
@@ -1,25 +1,47 @@
 import { TrackProps } from "@/types/tracks";
 
+/**
+ * Retrieves the value of the "track" item from the session storage.
+ * @returns The value of the "track" item, or null if it doesn't exist.
+ */
 export function getTrack() {
   return sessionStorage.getItem("track");
 }
 
-export function setTrack(track: TrackProps) {
+/**
+ * Saves the track object to the session storage.
+ * @param track - The track object to be saved.
+ */
+export function saveTrack(track: TrackProps) {
   sessionStorage.setItem("track", JSON.stringify(track));
 }
 
+/**
+ * Removes the "track" item from the sessionStorage.
+ */
 export function removeTrack() {
   sessionStorage.removeItem("track");
 }
 
+/**
+ * Retrieves the tracks from the session storage.
+ * @returns An array of TrackProps objects representing the tracks.
+ */
 export function getTracks(): TrackProps[] {
   return JSON.parse(sessionStorage.getItem("tracks") || "[]");
 }
 
-export function setTracks(tracks: TrackProps[]) {
+/**
+ * Saves the tracks to the session storage.
+ * @param tracks - An array of TrackProps objects representing the tracks to be saved.
+ */
+export function saveTracks(tracks: TrackProps[]) {
   sessionStorage.setItem("tracks", JSON.stringify(tracks));
 }
 
+/**
+ * Removes the "tracks" item from the session storage.
+ */
 export function removeTracks() {
   sessionStorage.removeItem("tracks");
 }
