7e6ffe86fd339e2e498e5f363c443f8ccb6cbe69
eben96 <pappy.rhutmannjnrolege@gmail.com>
2024-07-01 11:59:53 +0000

    refactor: Update playlist page to include playlist controls, image color styling, and tracklist data

--- components/shared/album-card.tsx
+++ components/shared/album-card.tsx
@@ -12,6 +12,7 @@ import { useState } from "react";
 import { cn } from "@/lib/utils";
 import { usePlayerContext } from "@/context/player-context";
 import { useAudioPlayer } from "@/hooks/use-audio-player";
+import { useRouter } from "next/navigation";
 
 type Props = {
   src: string;
@@ -19,12 +20,29 @@ type Props = {
   by?: string;
   onPlay?: () => void;
   id: string | number;
+  type: string;
 };
 
 export function AlbumCard({ onPlay, ...data }: Props) {
+  const router = useRouter();
+
   const [showPlay, setShowPlay] = useState(false);
   const { playing, play, pause, paused } = useAudioPlayer();
   const { currentTrack } = usePlayerContext();
+
+  function handlePlay() {
+    if (currentTrack?.id === data.id && playing) {
+      pause();
+      return;
+    }
+
+    if (currentTrack?.id === data.id && paused) {
+      play();
+      return;
+    }
+
+    onPlay?.();
+  }
 
   return (
     <button
@@ -36,7 +54,16 @@ export function AlbumCard({ onPlay, ...data }: Props) {
       }}
       className="flex w-full relative flex-col bg-white/5 rounded items-center justify-start px-0  text-sm gap-4"
     >
-      <Card className="flex w-full border-none flex-col h-full  items-center gap-2 justify-start">
+      <Card
+        onClick={() => {
+          if (data?.type === "track") {
+            handlePlay();
+            return;
+          }
+          router.push(`/${data.type}/${data.id}`);
+        }}
+        className="flex w-full border-none flex-col h-full  items-center gap-2 justify-start"
+      >
         <CardHeader className="flex px-4 w-full pb-0 flex-col gap-2 pt-4">
           <Image
             src={data?.src}
@@ -67,22 +94,8 @@ export function AlbumCard({ onPlay, ...data }: Props) {
         )}
       >
         <PlayButton
-          onClick={() => {
-            if (currentTrack?.id === data.id && playing) {
-              pause();
-              return;
-            }
-
-            if (currentTrack?.id === data.id && paused) {
-              play();
-              return;
-            }
-
-            onPlay?.();
-          }}
+          onClick={handlePlay}
           isPlaying={currentTrack?.id === data.id && playing}
-          size={"round"}
-          variant={"primary"}
           className="text-black w-10 h-10 md:h-14 md:w-14 lg:h-10 lg:w-10"
           iconSize={18}
         />
