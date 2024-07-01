b4452933a5be760db592443a2d671c80777bbdbd
eben96 <pappy.rhutmannjnrolege@gmail.com>
2024-07-01 16:06:42 +0000

    refactor: Update genre routes to fetch and return genre data

--- types/index.ts
+++ types/index.ts
@@ -2,6 +2,17 @@ export * from "./playlists";
 export * from "./tracks";
 export * from "./albums";
 
+export type GenreProps = {
+  id: string;
+  name: string;
+  picture: string;
+  picture_small: string;
+  picture_medium: string;
+  picture_big: string;
+  picture_xl: string;
+  type: string;
+};
+
 export type ApiResponse<T> = {
   results: T;
   message: string;
