54dc24a3a28ba0b28edf3d4adac3628cd0c20d6c
eben96 <pappy.rhutmannjnrolege@gmail.com>
2024-07-01 08:29:45 +0000

    refactor: Update .env.example and constants.ts to include NEXT_PUBLIC_BASE_URL for client-side API calls

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,5 @@
 export const ENV = {
   API_URL: process.env.DEEZER_BASE_URL ?? "https://api.deezer.com",
   API_KEY: process.env.DEEZER_API_KEY as string,
+  BASE_URL: process.env.NEXT_PUBLIC_BASE_URL ?? "http://localhost:3000",
 } as const;
--- types/index.ts
+++ types/index.ts
@@ -1,2 +1,8 @@
 export * from "./playlists";
 export * from "./tracks";
+
+export type ApiResponse<T> = {
+  results: T;
+  message: string;
+  status: number;
+};
