903481dcb1bf29256314a78acf3eeddc1c7cd6e7
eben96 <pappy.rhutmannjnrolege@gmail.com>
2024-07-01 12:25:24 +0000

    refactor: Update playlist page to include Recently Played section and improve image display

--- app/(app)/(lobby)/page.tsx
+++ app/(app)/(lobby)/page.tsx
@@ -2,9 +2,10 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
 import { ChevronDown, ChevronLeft, ChevronRight } from "lucide-react";
-import MiniPlaylist from "./_components/playlist/mini-playlist";
-import Tracks from "./_components/tracks";
 import Playlists from "./_components/playlist";
+import MiniPlaylist from "./_components/playlist/mini-playlist";
+import RecentlyPlayed from "./_components/recently-played/page";
+import Tracks from "./_components/tracks";
 
 export default function HomePage() {
   return (
@@ -45,22 +46,8 @@ export default function HomePage() {
 
             <Playlists />
           </div>
-          <div className="flex flex-col gap-4">
-            <div className="flex items-center justify-between">
-              <Label className="text-2xl font-bold">Recently played</Label>
-              <Button
-                variant={"ghost"}
-                className="text-sm text-muted-foreground"
-              >
-                View all
-              </Button>
-            </div>
-            <div className="grid gap-x-6 gap-y-4 md:grid-cols-3 lg:grid-cols-6">
-              {/* {Array.from({ length: 6 }).map((_, i) => (
-                <MusicCard key={i} />
-              ))} */}
-            </div>
-          </div>
+
+          <RecentlyPlayed />
         </div>
       </div>
     </main>
